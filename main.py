f02d36100242f7859fbc967151e31deebf4fa4c8
KMalyshe <kyryl.malyshev@gmail.com>
2023-10-26 20:17:41 +0200

    Why elif no worky

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ for i in range (0,n):
   current_line = "K"
   for i in range (0, abs(3-i)):
     current_line += " "
-  finished = current_line + "K "
+  finished = current_line + "K"
   if len(finished) < 6:
     for i in range (0, 6-len(finished)):
       finished += " "
+  finished += " "
   if i == 0:
     finished += "M   M"
   elif i == 1 or 2:
