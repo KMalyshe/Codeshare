59695774fe05c19d3018f77698ab580810c92fd5
KMalyshe <kyryl.malyshev@gmail.com>
2023-10-26 21:15:05 +0200

    it all worky

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# KYRYL MALYSHEV
+# K M
 # a
 n = 7
 for i in range (0,n):
   current_line = "K"
-  for i in range (0, abs(3-i)):
+  for j in range (0, abs(3-i)):
     current_line += " "
   finished = current_line + "K"
   if len(finished) < 6:
-    for i in range (0, 6-len(finished)):
+    for k in range (0, 6-len(finished)):
       finished += " "
   finished += " "
   if i == 0:
     finished += "M   M"
-  elif i == 1 or 2:
+  elif (i == 1) or (i == 2):
     finished += "MM MM"
   elif i == 3:
     finished += "M M M"
-  elif i == 4 or 5 or 6:
+  else:
     finished += "M   M"
   print(finished)
