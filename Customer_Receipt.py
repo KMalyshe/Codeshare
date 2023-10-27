4bc2f8065554098a3b4ffa5ec4d636b7726ab9f3
KMalyshe <kyryl.malyshev@gmail.com>
2023-10-27 09:28:46 +0200

    Customer_Receipt finished Initials finished

--- Customer_Receipt.py
+++ Customer_Receipt.py
@@ -1,41 +1,71 @@
-
-Pricedict = {"lovely_loveseat": 254.0, "stylish_settee": 180.5, "luxurious_lamp": 52.15}
-Descdict = {
-    "lovely_loveseat": "Lovely Loveseat. Tufted polyester blend on wood. "
-                       "32 inches high x 40 inches wide x 30 inches deep. Red or white.",
-    "stylish_settee": "Stylish Settee. Faux leather on birch. "
-                      "29.50 inches high x 54.75 inches wide x 28 inches deep. Black.",
-    "luxurious_lamp": "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+Items = {
+    "lovely_loveseat": [254.0, "\n Lovely Loveseat. Tufted polyester blend on wood. "
+                               "32 inches high x 40 inches wide x 30 inches deep. Red or white."],
+    "stylish_settee": [180.5, "\n Stylish Settee. Faux leather on birch. "
+                              "29.50 inches high x 54.75 inches wide x 28 inches deep. Black."],
+    "luxurious_lamp": [52.15, "\n Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."]
 }
 
-c1total: int = 0
-c1items: str = ""
+purchaselist: list = []  # List of purchases to be appended during program flow
+total: int = 0  # Tallying up
+desc: str = ""  # Tallying up
 
-def c1purchase(currtot, curritem, key):
+
+def purchase(plist, key):
     """
 
-    :param currtot: The current monetary total
-    :param curritem: The current itemized total
+    :param plist: The current receipt (short for purchaselist, as defined above)
     :param key: The item to be added, to be searched in the dictionary
-    :return: Returns the current monetary total and itemized total as a float and a string.
+    :return: Returns the current list of purchases, appended with the new item.
     """
-    currtot += Pricedict[key]
-    curritem += Descdict[key] + " \n"
-    return currtot, curritem
+    plist.append(Items[key])
 
 
-def c1tax(currtot):
+def tax(currtot):
     """
 
-    :param currtot:  Multiplies the current total by sales tax, used at the end.
     :return: Returns the total including tax.
     """
-    currtot *= 1.088
-    return currtot
+    return currtot * 1.088
 
 
-c1total, c1items = c1purchase(c1total, c1items, "lovely_loveseat")
-c1total, c1items = c1purchase(c1total, c1items, "luxurious_lamp")
-c1total = c1tax(c1total)
-print("Customer One Items: \n", c1items)
-print("Customer One Total:", c1total)
+def inputanalysis(inp):
+    """
+
+    :param inp: The string the user input
+    :return: Returns which item is meant accounting for capitalization
+    """
+    inp = inp[0:2]
+    inp = inp.lower()
+    if inp == "lo":
+        return "lovely_loveseat"
+    if inp == "lu":
+        return "luxurious_lamp"
+    if inp == "st":
+        return "stylish_settee"
+    return "unknown"
+
+cont = True
+# A sentinel boolean to control the following loop.
+while cont:
+    buy = input("Enter the item to be purchased. \n")
+    buy = inputanalysis(buy)
+    if buy == "unknown":
+        print("Invalid item. Please try again.")
+        continue
+    purchase(purchaselist, buy)
+    # Convert input to key accounting for capitalization of input and then add the purchase to the list
+
+    more = input("Are there more items? Y/N \n")
+    if more == "N":
+        cont = False
+    # Check if there are more items to be purchased.
+
+
+for index, element in enumerate(purchaselist):
+    total += element[0]
+    desc += element[1] + " "
+
+total = tax(total)
+print("Items:", desc)
+print("Total including tax:", total)
